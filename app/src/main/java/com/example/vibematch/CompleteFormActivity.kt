9e6e6f87220b8561158a40c9651be659bbd074af
SofyaLev <sofya_levchuk@bk.ru>
2025-05-09 11:59:08 +0300

    the form fields have been changed

--- app/src/main/java/com/example/vibematch/CompleteFormActivity.kt
+++ app/src/main/java/com/example/vibematch/CompleteFormActivity.kt
@@ -1,11 +1,6 @@
 package com.example.vibematch
-
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.textfield.MaterialAutoCompleteTextView
 
 class CompleteFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
